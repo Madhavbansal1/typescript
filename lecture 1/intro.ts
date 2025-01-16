c751f672f6e5a21967e89909048c46229ae917ac
Madhavbansal1 <madhavbansal6633@gmail.com>
2025-01-16 14:53:44 +0530

    lecture 2

---
+++ lecture 1/intro.ts
@@ -0,0 +1,5 @@
+ var user  = {name: "nadhav", age: 10 }
+console.log(user.name);
+
+
+// if we make a ts it create a js file of same name and run that file
---
+++ lecture2/intro.ts
@@ -0,0 +1,6 @@
+let greeting: String = "Hello, World!";
+
+greeting.trim();
+ // greeting = 1; gives error because greeting is of type string
+
+console.log(greeting);
